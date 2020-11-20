6b2aa31348a614178d85aea2399f140d3f338956
MatryxSoftGitHub <51738073+MatryxSoftGitHub@users.noreply.github.com>
2020-11-20 15:04:05 +0530

    Update ExecutionBat.bat

--- ExecutionBat.bat
+++ ExecutionBat.bat
@@ -3,5 +3,5 @@ for /f "tokens=3,2,4 delims=/- " %%x in ("%date%") do set d=%%y%%x%%z
 set data=%d%
 Echo zipping...
 "C:\Program Files\7-Zip\7z.exe" a -tzip "D:\JenkinsHome\workspace\JMeter_Netmeds\HtmlReport.zip" "D:\JenkinsHome\workspace\JMeter_Netmeds\HtmlReport\*.*"
-TIMEOUT /T 10
+
 echo Done!
